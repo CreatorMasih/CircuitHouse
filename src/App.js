f2f60f8b6a0258e933ba9fb4417821f6a0a5aa96
CreatorMasih <161126324+CreatorMasih@users.noreply.github.com>
2024-10-28 15:56:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from './components/Main'
-import BookRoom  from "./components/BookRoom.js";
+import BookRoom  from "./components/BookRoom";
 import Admin from "./components/Admin";
 import Login from "./components/Login";
 
